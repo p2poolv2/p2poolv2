16d31e8b322e2fc6455ff1ca979ac4a2d5d5e206
pool2win <pool2win@users.noreply.github.com>
2025-01-27 21:24:40 +0100

    Increase waits in integration tests
    We could devise a smarter way to do this, but am leaving it as a future optimisation

--- tests/receive_shares_working_from_ckpool_test.rs
+++ tests/receive_shares_working_from_ckpool_test.rs
@@ -54,7 +54,7 @@ mod zmq_tests {
             .expect("Failed to bind ZMQ socket");
 
         // Give the node time to start up
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
 
         // Deserialize into MinerMessage array
         let ckpool_messages: Vec<CkPoolMessage> =
@@ -69,7 +69,7 @@ mod zmq_tests {
                 .expect("Failed to publish message");
 
             // Small delay between messages to avoid overwhelming the system
-            tokio::time::sleep(Duration::from_millis(10)).await;
+            tokio::time::sleep(Duration::from_millis(100)).await;
         }
 
         let workbase = chain_handle
@@ -123,7 +123,7 @@ mod zmq_tests {
             .expect("Failed to bind ZMQ socket");
 
         // Give the node time to start up
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
 
         // Deserialize into MinerMessage array
         let ckpool_messages: Vec<CkPoolMessage> =
@@ -201,7 +201,7 @@ mod zmq_tests {
             .expect("Failed to bind ZMQ socket");
 
         // Give the node time to start up
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
 
         // Deserialize into MinerMessage array
         let ckpool_messages: Vec<CkPoolMessage> =
