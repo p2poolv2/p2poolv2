c52425b836f04c1a0af572245124359c9793fe13
pool2win <pool2win@users.noreply.github.com>
2025-03-27 18:03:58 +0100

    Add p2pool testnet4 genesis block

--- src/shares/genesis.rs
+++ src/shares/genesis.rs
@@ -43,10 +43,23 @@ const SIGNET_GENESIS_DATA: GenesisData = GenesisData {
     bitcoin_blockhash: "000000000822bbfaf34d53fc43d0c1382054d3aafe31893020c315db8b0a19f9",
 };
 
+const TESTNET4_GENESIS_DATA: GenesisData = GenesisData {
+    workinfoid: 0,
+    clientid: 0,
+    enonce1: "771de467",
+    nonce2: "0000000000000000",
+    nonce: "74927904",
+    ntime: 1743003095, // u32::from_str_radix("67b6f938", 16).unwrap(),
+    diff: dec!(1.0),
+    sdiff: dec!(4.0170006421734943),
+    bitcoin_blockhash: "000000003fba69400bbc385acd52b07dbe7779ea5f8995dd4aadf4a86b74cc55",
+};
+
 /// Get the genesis data for a given network
 pub fn genesis_data(network: bitcoin::Network) -> Result<GenesisData, Box<dyn Error>> {
     match network {
         bitcoin::Network::Signet => Ok(SIGNET_GENESIS_DATA),
+        bitcoin::Network::Testnet4 => Ok(TESTNET4_GENESIS_DATA),
         _ => Err("Unsupported network".into()),
     }
 }
@@ -69,6 +82,23 @@ mod tests {
         assert_eq!(
             genesis.bitcoin_blockhash,
             "000000000822bbfaf34d53fc43d0c1382054d3aafe31893020c315db8b0a19f9"
+        );
+    }
+
+    #[test]
+    fn test_testnet4_genesis_data() {
+        let genesis = genesis_data(bitcoin::Network::Testnet4).unwrap();
+        assert_eq!(genesis.workinfoid, 0);
+        assert_eq!(genesis.clientid, 0);
+        assert_eq!(genesis.enonce1, "771de467");
+        assert_eq!(genesis.nonce2, "0000000000000000");
+        assert_eq!(genesis.nonce, "74927904");
+        assert_eq!(genesis.ntime, 1743003095);
+        assert_eq!(genesis.diff, dec!(1.0));
+        assert_eq!(genesis.sdiff, dec!(4.0170006421734943));
+        assert_eq!(
+            genesis.bitcoin_blockhash,
+            "000000003fba69400bbc385acd52b07dbe7779ea5f8995dd4aadf4a86b74cc55"
         );
     }
 }
