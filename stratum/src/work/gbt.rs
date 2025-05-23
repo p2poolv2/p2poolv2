7a21826f4e80979efe96d295c67c19a51b427033
pool2win <pool2win@users.noreply.github.com>
2025-05-23 07:16:37 +0200

    Use ckpool notify as merkle branches test data

--- stratum/src/work/gbt.rs
+++ stratum/src/work/gbt.rs
@@ -102,44 +102,10 @@ async fn get_block_template<R: BitcoindRpc>(
     }
 }
 
-// /// Start a task to fetch block templates from bitcoind
-// pub async fn start_gbt<B: BitcoindRpc + Sync + Send + 'static>(
-//     url: String,
-//     username: String,
-//     password: String,
-//     result_tx: tokio::sync::mpsc::Sender<BlockTemplate>,
-// ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-//     tokio::spawn(async move {
-//         let bitcoind = match B::new(url, username, password) {
-//             Ok(bitcoind) => bitcoind,
-//             Err(e) => {
-//                 info!("Failed to connect to bitcoind: {}", e);
-//                 return;
-//             }
-//         };
-//         loop {
-//             match get_block_template(&bitcoind).await {
-//                 Ok(template) => {
-//                     debug!("Block template: {:?}", template);
-//                     if result_tx.send(template).await.is_err() {
-//                         info!("Failed to send block template to channel");
-//                     }
-//                 }
-//                 Err(e) => {
-//                     info!("Error getting block template: {}", e);
-//                 }
-//             };
-//         }
-//     })
-//     .await;
-//     Err(Box::new(WorkError {
-//         message: "Failed to start GBT".to_string(),
-//     }))
-// }
-
 #[cfg(test)]
 mod gbt_load_tests {
     use super::*;
+    use bitcoin::hex::FromHex;
     use bitcoindrpc::MockBitcoindRpc;
 
     #[tokio::test]
@@ -176,30 +142,64 @@ mod gbt_load_tests {
             )
         )
     }
+
+    // The test data comes from tests/test_data/gbt/regtest/ckpool/one-txn dir
     #[test_log::test]
     fn test_compute_merkle_branches_single_txid() {
         // Only one txid, branch should be just that txid
-        let txid = sha256d::Hash::hash(&[1u8; 32]);
+        let txid: sha256d::Hash =
+            "2305d23e3d6a9f55189723e0dfc653908455a9162b263a0b7b584711cff8cdfe"
+                .parse()
+                .unwrap();
         let branches = compute_merkle_branches(vec![txid]);
         assert_eq!(branches, vec![txid]);
     }
 
+    // The test data comes from tests/test_data/gbt/regtest/ckpool/two-txns dir
     #[test]
     fn test_compute_merkle_branches_two_txids() {
-        let txid1 = sha256d::Hash::hash(&[1u8; 32]);
-        let txid2 = sha256d::Hash::hash(&[2u8; 32]);
+        let txid1: sha256d::Hash =
+            "9a1644beab4dfaccc9bb3cbd5008b0581dd314102fcd79b69a2c724a8010d706"
+                .parse()
+                .unwrap();
+        let txid2: sha256d::Hash =
+            "3bca24fa1c1bc8cfcf77dfd974bb5c80ea865caa828c2e43f86cea862d5d6e40"
+                .parse()
+                .unwrap();
+
         let expected_hash = sha256d::Hash::hash(&[txid2, txid2].concat());
         let branches = compute_merkle_branches(vec![txid1, txid2]);
         assert_eq!(branches.len(), 2);
         assert_eq!(branches[0], txid1);
         assert_eq!(branches[1], expected_hash);
+
+        // The expected merkle branches from ckpool's notify messages
+        let v1 = Vec::from_hex("06d710804a722c9ab679cd2f1014d31d58b00850bd3cbbc9ccfa4dabbe44169a")
+            .unwrap();
+        let v2 = Vec::from_hex("dfa3f22130ce2941d79b5f6c35d15f15688910e49b4d7e460f07cb44c925ca49")
+            .unwrap();
+        let expected: Vec<sha256d::Hash> = vec![
+            sha256d::Hash::from_slice(&v1).unwrap(),
+            sha256d::Hash::from_slice(&v2).unwrap(),
+        ];
+        assert_eq!(branches, expected);
     }
 
+    // The test data comes from tests/test_data/gbt/regtest/ckpool/three-txns dir
     #[test]
     fn test_compute_merkle_branches_three_txids() {
-        let txid1 = sha256d::Hash::hash(&[1u8; 32]);
-        let txid2 = sha256d::Hash::hash(&[2u8; 32]);
-        let txid3 = sha256d::Hash::hash(&[3u8; 32]);
+        let txid1: sha256d::Hash =
+            "4193df037a4d8ffb9815f2fdae19abab69821f06625004a1e84d8df73cc0c044"
+                .parse()
+                .unwrap();
+        let txid2: sha256d::Hash =
+            "082af095391dbb4d8918765f41524e010ee3838adf3b3b13f280a6c83cad7776"
+                .parse()
+                .unwrap();
+        let txid3: sha256d::Hash =
+            "58dd52c691e92e6e9da0be3146dfdc87c2c7c1ab6e64476f26427c6d771de6a5"
+                .parse()
+                .unwrap();
 
         let h23 = sha256d::Hash::hash(&[txid2, txid3].concat());
         let branches = compute_merkle_branches(vec![txid1, txid2, txid3]);
@@ -207,14 +207,38 @@ mod gbt_load_tests {
         assert_eq!(branches[0], txid1);
         assert_eq!(branches[1], h23);
         assert_eq!(branches.len(), 2);
+
+        // The expected merkle branches from ckpool's notify messages
+        let v1 = Vec::from_hex("44c0c03cf78d4de8a1045062061f8269abab19aefdf21598fb8f4d7a03df9341")
+            .unwrap();
+        let v2 = Vec::from_hex("0dce56f68943df6531358e75bcad1e49e2456413cfd03244beaabc8cc9b69ee3")
+            .unwrap();
+        let expected: Vec<sha256d::Hash> = vec![
+            sha256d::Hash::from_slice(&v1).unwrap(),
+            sha256d::Hash::from_slice(&v2).unwrap(),
+        ];
+        assert_eq!(branches, expected);
     }
 
+    // The test data comes from tests/test_data/gbt/regtest/ckpool/three-txns dir
     #[test]
     fn test_compute_merkle_branches_four_txids() {
-        let txid1 = sha256d::Hash::hash(&[1u8; 32]);
-        let txid2 = sha256d::Hash::hash(&[2u8; 32]);
-        let txid3 = sha256d::Hash::hash(&[3u8; 32]);
-        let txid4 = sha256d::Hash::hash(&[4u8; 32]);
+        let txid1: sha256d::Hash =
+            "19d9fe2c24a7b88b4d0455c9f42ce16eec881a9fc2cf1194a554129f8e297c27"
+                .parse()
+                .unwrap();
+        let txid2: sha256d::Hash =
+            "c3e8a4a3405cafc0ac48a1c602cf6962c74f180dccbd7a423a9216497dcb474e"
+                .parse()
+                .unwrap();
+        let txid3: sha256d::Hash =
+            "1b991d93da9c481637d7285ec378d4b1fd6b1ccb06fed594951f3705b3869ddb"
+                .parse()
+                .unwrap();
+        let txid4: sha256d::Hash =
+            "13a0ba57fdd05ce8b89950f792d8849ee636831a77d9f1eb1fda38e68991a2f0"
+                .parse()
+                .unwrap();
 
         let h23 = sha256d::Hash::hash(&[txid2, txid3].concat());
         let h44 = sha256d::Hash::hash(&[txid4, txid4].concat());
@@ -225,5 +249,19 @@ mod gbt_load_tests {
         assert_eq!(branches[1], h23);
         assert_eq!(branches[2], h4444);
         assert_eq!(branches.len(), 3);
+
+        // The expected merkle branches from ckpool's notify messages
+        let v1 = Vec::from_hex("277c298e9f1254a59411cfc29f1a88ec6ee12cf4c955044d8bb8a7242cfed919")
+            .unwrap();
+        let v2 = Vec::from_hex("20838512651774fd1482b92f7d55e14103e773b92721873a29bb06846f297683")
+            .unwrap();
+        let v3 = Vec::from_hex("50c833466187becdc8e2f069352e7be34da07d653793093861c913eef295285c")
+            .unwrap();
+        let expected: Vec<sha256d::Hash> = vec![
+            sha256d::Hash::from_slice(&v1).unwrap(),
+            sha256d::Hash::from_slice(&v2).unwrap(),
+            sha256d::Hash::from_slice(&v3).unwrap(),
+        ];
+        assert_eq!(branches, expected);
     }
 }
