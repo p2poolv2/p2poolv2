36c3f061b70d230ceebcdd71a11dae8a307b9962
pool2win <pool2win@users.noreply.github.com>
2025-05-26 10:25:32 +0200

    Remove gossipsub peers on disconnect

--- p2poolv2_lib/src/node/behaviour/mod.rs
+++ p2poolv2_lib/src/node/behaviour/mod.rs
@@ -130,9 +130,12 @@ impl P2PoolBehaviour {
         self.gossipsub.add_explicit_peer(&peer_id);
     }
 
+    /// Remove the peer from Kademlia's routing table,
+    /// gossipsub's explicit peers, and request/response protocols
     pub fn remove_peer(&mut self, peer_id: &PeerId) {
-        // Remove the peer from Kademlia's routing table
         self.kademlia.remove_peer(peer_id);
+        self.gossipsub.remove_explicit_peer(peer_id);
+        // self.request_response.remove_address(peer, address);
     }
 }
 
