d71f512b8abdc6c0e5b58613415a0e515aec569d
SurajNaidu0 <surajangurajnaidu@gmail.com>
2025-07-29 14:44:09 +0530

    fixed onchain initiate amount

--- p2poolv2_atomic_swap/src/cli/atomic_swap_functions.rs
+++ p2poolv2_atomic_swap/src/cli/atomic_swap_functions.rs
@@ -3,13 +3,9 @@ use crate::configuration::HtlcConfig;
 use crate::htlc::{generate_htlc_address, redeem_htlc_address, refund_htlc_address};
 use crate::lightning_node::{getinvoice, onchaintransfer, payinvoice,getaddress};
 use crate::swap::{create_swap,retrieve_swap, Swap, HTLCType, Bitcoin, Lightning};
-use futures::future::err;
-use ldk_node::bitcoin::Txid;
-use ldk_node::lightning::blinded_path::payment;
 use ldk_node::lightning_invoice::Bolt11Invoice;
-use ldk_node::payment::{PaymentKind, PaymentStatus};
+use ldk_node::payment::{PaymentKind};
 use ldk_node::Node;
-use std::str::FromStr;
 use std::{thread, time::Duration};
 use crate::bitcoin::checks::{filter_valid_htlc_utxos,filter_refundable_utxos};
 use crate::lightning_node::checks::is_invoice_payable_simple;
@@ -45,12 +41,12 @@ pub async fn initiate_onchain_to_lightning_swap(node : &Node,db_path:&str,initia
     };
 
     //uploading this swap to db 
-    let swap_result= create_swap(&swap, db_path).expect("Error in creating a swap");
+    let _swap_result= create_swap(&swap, db_path).expect("Error in creating a swap");
 
     //sending onchain bitcoin to the htlc address 
     let htlc_address = generate_htlc_address(&swap).expect("Error in creating a address");
 
-    let txid =onchaintransfer(node, &htlc_address, to_amount).await.expect("Error in getting txid");
+    let txid =onchaintransfer(node, &htlc_address, from_amount).await.expect("Error in getting txid");
 
     println!("Swap created {:?}", swap);
     println!("Lightining invoice {}",invoice);
@@ -164,7 +160,7 @@ pub async fn redeem_swap(
 
             let payment_kind = node.payment(&payment_id).expect("Errror in getting payment id").kind;
 
-            if let PaymentKind::Bolt11 { hash, preimage, secret, .. } = payment_kind{
+            if let PaymentKind::Bolt11 {  preimage, .. } = payment_kind{
                 let preimage = preimage.expect("error in getting preimage");
                 //calling redeem 
                 let raw_tx = redeem_htlc_address(&swap, preimage.to_string().as_str(), private_key.as_str(), utxos, &_funding_address).expect("error in sending trx");
@@ -236,135 +232,3 @@ pub async fn refund_swap(
 
 
 
-// async fn initiate_bitcoin_wait_lightining(swap_id: &str, db_path: &str, node: &Node) {
-//     //reterving swap ffrom db if the swap key is not fond returning error
-//     let swap = retrieve_swap(db_path, swap_id)
-//         .expect("Error in fetching data from db")
-//         .expect("swap id not found in database");
-
-//     //TODO
-//     // need to implement safty checks ensuring initiator doesnt pay twice or more that intended amount
-
-//     //getting lighting invoice
-//     //TODO
-//     //as of now it uses default cltv time lock of 24
-//     let amount_msats = swap.to_chain.amount * 1000; //sats to msats
-//     let invoice = getinvoice(node, amount_msats)
-//         .await
-//         .expect("error in getting invoice");
-
-//     //getting htlc address
-//     let htlc_address = generate_htlc_address(&swap).expect("error in getting htlc address");
-
-//     // paying from about to it
-//     //TODO
-//     //As of now this payes only through the wallet in node
-//     //this can be improved with a inbuild wallet or just showing address to to the insiter so he can use any wallet
-//     let amount_sats = swap.from_chain.amount;
-//     let txid = onchaintransfer(node, &htlc_address, amount_sats)
-//         .await
-//         .expect("error in initiating transection");
-// }
-
-// async fn initiate_lightning_redeem_bitcoin(
-//     swap_id: &str,
-//     db_path: &str,
-//     node: &Node,
-//     htlc_config: &HtlcConfig,
-//     invoice: &Bolt11Invoice,
-// ) {
-//     //reterving swap ffrom db if the swap key is not fond returning error
-//     let swap = retrieve_swap(db_path, swap_id)
-//         .expect("Error in fetching data from db")
-//         .expect("swap id not found in database");
-
-//     //TODO change the rpc url to get from chain config rather than from htlc config in version 2
-
-//     let rpc_url = &htlc_config.rpc_url;
-
-//     //getting htlc address
-//     let address = generate_htlc_address(&swap).expect("error in getting htlc address");
-
-//     //getting utxos
-//     let utxos = fetch_utxos_for_address(rpc_url, &address)
-//         .await
-//         .expect("error in getting utxo");
-
-//     //TODO: make to function to feth this wen adding features of constuting mutiple inputs
-
-//     //as of now taking only first utxo
-//     let utxo = utxos[0];
-
-//     let txid = Txid::from_str(utxo.txid.as_str()).expect("error in making txid");
-//     let vout = utxo.vout;
-
-//     //checking source initiate condition
-//     //TODO - need to chneg this function to get utxo from this call not fetch inside the function and same for valaditing lightning invoice
-//     let from_chain_check = check_bitcoin_from_initiate(swap, htlc_config).await;
-
-//     if from_chain_check == false {
-//         err("from_chain checks failed")
-//     }
-
-//     //valaditing lightning invoice
-//     let to_chain_check = check_lighting_invoice_to_payable(swap, htlc_config, invoice).await;
-
-//     if from_chain_check == false {
-//         err("to_chain checks failed")
-//     }
-
-//     //if both passed
-//     //we pay through out node
-//     let payment_id = payinvoice(node, &invoice)
-//         .await
-//         .expect("error sending payment");
-
-//     // getiing secreat and handlling swap can be done way better using a different stuctue cassacily using even_handling in lightining node but this will be relised in future vertion of the create as of now we are just going to chak payment id statf  for 1 min
-
-//     let mut counter = 0;
-
-//     loop {
-//         println!("Check number: {}", counter);
-
-//         if counter >= 5 {
-//             println!("secrate is not reviled in a min please try doin mainy redeem using redeem from hash sunch in lighitngn node ");
-//             break;
-//         }
-
-//         let payment_status = node
-//             .payment(&payment_id)
-//             .expect("error in getting payment status");
-
-//         match payment_status.status {
-//             PaymentStatus::Succeeded => {
-//                 println!("Payment Sucessfull");
-//                 if let PaymentKind::Bolt11 {
-//                     hash,
-//                     preimage,
-//                     secret,
-//                 } = payment_status.kind
-//                 {
-//                     // payment is sucessfull and redeeming bitcoin
-//                     // constructing redeem bytes
-//                     redeem_htlc_address(
-//                         &swap,
-//                         preimage,
-//                         htlc_config,
-//                         amount,
-//                         prev_txid,
-//                         vout,
-//                         transfer_to_address,
-//                     );
-//                 }
-//                 break;
-//             }
-//             PaymentStatus::Pending => {}
-//             PaymentStatus::Failed => {}
-//         }
-
-//         counter += 1;
-//         thread::sleep(Duration::from_secs(10));
-//     }
-// }
-
-// // fn refund_bicoin()
