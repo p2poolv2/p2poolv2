f4ff73d059a1d378f511f2b9fddd6696bad38ce1
Johnny Santos <johnnyadsantos@gmail.com>
2026-01-08 16:05:48 -0300

    feat(docker): improve local cluster
    The compose file didn't match latest paths for volumes. Changed it so it
    would properly show in the docker volumes on inspecting them.
    
    Made the bitcoind image multistage to reduce from almost ~3gb to ~180mb.

--- docker/cpuminer-startup.sh
+++ docker/cpuminer-startup.sh
@@ -1,52 +1,83 @@
 #!/bin/bash
 
-HOST=bitcoind
-MINERD=/cpuminer/minerd
-TARGET_HEIGHT=32 # We need to wait for the height to reach 16 to get past the ckpool ser_num issue, but we overshoot to 32 for extra safety
+set -euxo pipefail
 
-# Default to signet if no network is specified
-NETWORK=${1:-signet}
+BTC_RPC_HOST=${BTC_RPC_HOST:-${HOST:-bitcoind}}
+BTC_RPC_USER=${BTC_RPC_USER:-p2pool}
+BTC_RPC_PASSWORD=${BTC_RPC_PASSWORD:-p2pool}
+BTC_NETWORK=${1:-${BTC_NETWORK:-signet}}
+P2POOL_STRATUM_HOST=${P2POOL_STRATUM_HOST:-p2pool}
+P2POOL_STRATUM_PORT=${P2POOL_STRATUM_PORT:-3333}
+
+MINERD=/cpuminer/minerd
+TARGET_HEIGHT=${TARGET_HEIGHT:-32} # We need to wait for the height to reach 16 to get past the ckpool ser_num issue, but we overshoot to 32 for extra safety
+
+trap cleanup EXIT
+trap cleanup INT
+
+function cleanup() {
+  echo "Cleaning up..."
+  killall -9 $MINERD
+  exit 0
+}
 
 # Set ports based on network
-if [ "$NETWORK" = "signet" ]; then
-  BTC_P2P_PORT=38333
-  BTC_RPC_PORT=38332
-  ADDR="tb1qyazxde6558qj6z3d9np5e6msmrspwpf6k0qggk"
-elif [ "$NETWORK" = "testnet4" ]; then
-  BTC_P2P_PORT=48333
-  BTC_RPC_PORT=48332
-  ADDR="tb1q0afww6y0kgl4tyjjyv6xlttvfwdfqxvrfzz35f"
+if [ "$BTC_NETWORK" = "signet" ]; then
+  BTC_P2P_PORT=${BTC_P2P_PORT:-38333}
+  BTC_RPC_PORT=${BTC_RPC_PORT:-38332}
+  ADDR=${ADDR:-tb1qyazxde6558qj6z3d9np5e6msmrspwpf6k0qggk}
+elif [ "$BTC_NETWORK" = "testnet4" ]; then
+  BTC_P2P_PORT=${BTC_P2P_PORT:-48333}
+  BTC_RPC_PORT=${BTC_RPC_PORT:-48332}
+  ADDR=${ADDR:-tb1q0afww6y0kgl4tyjjyv6xlttvfwdfqxvrfzz35f}
 else
-  echo "Unsupported network: $NETWORK. Use 'signet' or 'testnet4'."
+  echo "Unsupported network: $BTC_NETWORK. Use 'signet' or 'testnet4'."
   exit 1
 fi
 
 # Wait for bitcoind to be ready
 echo "Waiting for bitcoind..."
-while ! nc -z $HOST $BTC_RPC_PORT; do
+while ! nc -z $BTC_RPC_HOST $BTC_RPC_PORT; do
   sleep 1
 done
 echo "Bitcoind is up"
 
 
+function getheight() {
+  curl -s --user "$BTC_RPC_USER:$BTC_RPC_PASSWORD" --data-binary '{"jsonrpc":"1.0","id":"1","method":"getblockcount","params":[]}' http://$BTC_RPC_HOST:$BTC_RPC_PORT | grep -o '"result":[0-9]*' | cut -d':' -f2
+}
+
 # Query bitcoin height
-height=$(curl -s --user p2pool:p2pool --data-binary '{"jsonrpc":"1.0","id":"1","method":"getblockcount","params":[]}' http://$HOST:$BTC_RPC_PORT | grep -o '"result":[0-9]*' | cut -d':' -f2)
+height=$(getheight)
 
 echo "Current block height: $height"
 
 if [ "$height" -lt $TARGET_HEIGHT ]; then
     echo "Height below $TARGET_HEIGHT, using direct mining without stratum"
-    $MINERD --algo=sha256d --url=http://p2pool:p2pool@$HOST:$BTC_RPC_PORT --userpass=p2pool:p2pool --coinbase-addr=$ADDR --no-stratum --background &
+    $MINERD --algo=sha256d \
+        --url=http://${BTC_RPC_HOST}:${BTC_RPC_PORT} \
+        --userpass="${BTC_RPC_USER}:${BTC_RPC_PASSWORD}" \
+        --coinbase-addr=$ADDR \
+        --no-stratum \
+        --protocol-dump \
+        --background &
     # Wait for height to reach 16
     while [ "$height" -lt $TARGET_HEIGHT ]; do
-        height=$(curl -s --user p2pool:p2pool --data-binary '{"jsonrpc":"1.0","id":"1","method":"getblockcount","params":[]}' http://$HOST:$BTC_RPC_PORT | grep -o '"result":[0-9]*' | cut -d':' -f2)
+        height=$(getheight)
         echo "Current block height: $height"
     done
-    # Kill cpuminer when done
-    killall minerd
+    echo "Height reached: $height"
+    cleanup
 fi
 
-echo "Height reached: $height"
-
 # Start minerd
-exec /cpuminer/minerd --algo=sha256d --url=stratum+tcp://ckpool:3333 --user=$ADDR --pass=x --debug -l 7
+exec $MINERD \
+    --algo=sha256d \
+    --url=stratum+tcp://$P2POOL_STRATUM_HOST:$P2POOL_STRATUM_PORT \
+    --user=$ADDR \
+    --pass=x \
+    --debug \
+    --protocol-dump \
+    --retry-pause=10 \
+    --retries=100 \
+    --threads 1
