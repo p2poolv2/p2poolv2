4303f594fa60d365331b2934d2e454f3171c1cd2
SurajNaidu0 <surajangurajnaidu@gmail.com>
2026-01-18 15:43:03 +0530

    Fix: Change the share value to 1 BTC so that, in the future, a portion of the shares can be used to pay fees in the transaction engine.

--- p2poolv2_lib/src/shares/share_block/mod.rs
+++ p2poolv2_lib/src/shares/share_block/mod.rs
@@ -369,7 +369,7 @@ mod tests {
         assert_eq!(share.transactions[0].input.len(), 1);
 
         let output = &share.transactions[0].output[0];
-        assert_eq!(output.value.to_sat(), 1);
+        assert_eq!(output.value.to_sat(), 100_000_000);
 
         let expected_address = bitcoin::Address::p2pkh(
             "02ac493f2130ca56cb5c3a559860cef9a84f90b5a85dfe4ec6e6067eeee17f4d2d"
@@ -400,7 +400,7 @@ mod tests {
 
         // Verify the output is a P2PKH to the miner's public key
         let output = &share_block.transactions[0].output[0];
-        assert_eq!(output.value.to_sat(), 1);
+        assert_eq!(output.value.to_sat(), 100_000_000);
 
         // Verify the output script is P2PKH for the miner's pubkey
         let expected_address = bitcoin::Address::p2pkh(pubkey, bitcoin::Network::Regtest);
